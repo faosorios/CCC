ae410619aff3d62f8a7578638a261df278572cb2
Felipe Osorio <faosorios@gmail.com>
2018-04-20 17:10:09 -0300

    Add files via upload

--- simulation/ccc.simul.R
+++ simulation/ccc.simul.R
@@ -1,14 +1,14 @@
 ## Id: ccc.simul.R
 ## Author: originally coded by Carla Leal, with contributions of Felipe Osorio
-## Last update: 2018-04-12
+## Last update: 2018-04-20
 
 simul.ccc <- function(Nsize = 500, nobs = 100, mean, Sigma, delta = 3)
 { ## function to perform the simulation experiment (Section 4 of the manuscript)
-  ok <- matrix(FALSE, nrow = Nsize, ncol = 6) # results container
-  cutoff <- rep(0, 5)
+  ok <- matrix(FALSE, nrow = Nsize, ncol = 5) # results container
+  cutoff <- rep(0, 4)
   now <- proc.time()
 
-  # Monte Carlo runs
+  # Monte Carlo iterations
   for (i in 1:Nsize) {
     x <- rmnorm(n = nobs, mean = mean, Sigma = Sigma)
     # introducing an outlier to 2nd instrument
@@ -25,29 +25,24 @@ simul.ccc <- function(Nsize = 500, nobs = 100, mean, Sigma, delta = 3)
     hmax <- abs(z$hmax)
     cutoff[2] <- mean(hmax) + 2 * sd(hmax)
     ok[i,2] <- hmax[1] > cutoff[2]
-    # largest slope
-    z <- influence.ccc(fm, method = "CF")
-    hmax <- abs(z$hmax)
-    cutoff[3] <- mean(hmax) + 2 * sd(hmax)
-    ok[i,3] <- hmax[1] > cutoff[3]
     # FI measure
     z <- influence.ccc(fm, method = "FI")
     hmax1 <- abs(z$hmax)
-    cutoff[4] <- mean(hmax1) + 2 * sd(hmax1)
-    ok[i,4] <- hmax1[1] > cutoff[4]
+    cutoff[3] <- mean(hmax1) + 2 * sd(hmax1)
+    ok[i,3] <- hmax1[1] > cutoff[3]
     # SI measure
     z <- influence.ccc(fm, method = "conformal")
     hmax2 <- abs(z$hmax)
-    cutoff[5] <- mean(hmax2) + 2 * sd(hmax2)
-    ok[i,5] <- hmax2[1] > cutoff[5]
+    cutoff[4] <- mean(hmax2) + 2 * sd(hmax2)
+    ok[i,4] <- hmax2[1] > cutoff[4]
     # FI & SI measures
-    ok[i,6] <- (hmax1[1] > cutoff[4]) && (hmax2[1] > cutoff[5])
+    ok[i,5] <- (hmax1[1] > cutoff[3]) && (hmax2[1] > cutoff[4])
   }
 
-  mnames <- c("C","B","CF","FI","SI","both")
+  mnames <- c("C","B","FI","SI","both")
   percentage <- apply(ok, 2, sum) / Nsize
   names(percentage) <- mnames
-  names(cutoff) <- mnames[1:5]
+  names(cutoff) <- mnames[1:4]
   speed <- proc.time() - now
   out <- list(percentage = 100 * percentage, cutoffs = cutoff, speed = speed, x = x, hmax = hmax)
   out
--- simulation/simulation.R
+++ simulation/simulation.R
@@ -11,6 +11,7 @@ mu    <- c(0,0)
 Sigma <- matrix(c(1,.95,.95,1), ncol = 2)
 
 ## creating Table 2..
+# seeds: 5, 23, 53, 167, 239, 347, 433 (OMG! all are prime numbers...)
 
 ## 1st column: delta = 0.5
 set.seed(5)
@@ -23,7 +24,7 @@ out <- simul.ccc(mean = mu, Sigma = Sigma, delta = 1.0)
 out[c("percentage","cutoffs","speed")] # just 'percentages' are reported at Table 2
 
 ## 3rd column: delta = 1.5
-set.seed(51)
+set.seed(53)
 out <- simul.ccc(mean = mu, Sigma = Sigma, delta = 1.5)
 out[c("percentage","cutoffs","speed")] # just 'percentages' are reported at Table 2
 
@@ -31,7 +32,7 @@ out[c("percentage","cutoffs","speed")] # just 'percentages' are reported at Tabl
 set.seed(167)
 out <- simul.ccc(mean = mu, Sigma = Sigma, delta = 2.0)
 out[c("percentage","cutoffs","speed")] # just 'percentages' are reported at Table 2
-x <- out$x # saving a typical dataset for posterior analyses
+x <- out$x # saving a 'typical' dataset for posterior analyses
 
 ## 5th column: delta = 2.5
 set.seed(239)
@@ -43,22 +44,28 @@ set.seed(347)
 out <- simul.ccc(mean = mu, Sigma = Sigma, delta = 3.0)
 out[c("percentage","cutoffs","speed")] # just 'percentages' are reported at Table 2
 
+## 7th column: delta = 3.5
+set.seed(347)
+out <- simul.ccc(mean = mu, Sigma = Sigma, delta = 3.0)
+out[c("percentage","cutoffs","speed")] # just 'percentages' are reported at Table 2
+
 ## Figure 4
 par(pty = "s")
 plot(x, xlim = c(-3.5,3.5), ylim = c(-3.5,3.5), xlab = "X1", ylab = "X2")
 abline(c(0,1), col = "red", lty = 2, lwd = 2)
-text(x[1,1], x[1,2], as.character(1), pos = 3)
+obs <- c(1,15,27,46,60)
+text(x[obs,1], x[obs,2], as.character(obs), pos = 3)
 
 ## fitting postulated model
 fm <- fit.ccc(x)
 
 # Fig 5.a
 z  <- influence.ccc(fm, method = "normal")
-plot(z)
+plot(z, idn = 4)
 
 # Fig 5.b
 z  <- influence.ccc(fm, method = "conformal")
-plot(z)
+plot(z, idn = 4)
 
 # Fig 5.c
 z  <- influence.ccc(fm, method = "FI")
@@ -67,3 +74,28 @@ plot(z)
 # Fig 5.d
 z  <- influence.ccc(fm, method = "SI")
 plot(z)
+
+## Fig 6
+nobs <- 100 # nrow(x)
+rhoc <- rep(0, nobs)
+accu <- rep(0, nobs)
+r    <- rep(0, nobs)
+for (i in 1:nobs) {
+  f0 <- fit.ccc(x, subset = -i)
+  rhoc[i] <- f0$ccc
+  accu[i] <- f0$accuracy
+  r[i]    <- f0$precision
+}
+par(mfrow = c(3,1))
+plot(rhoc, type = "b", ylim = c(.935,.965), ylab = "CCC estimate")
+abline(h = fm$ccc, col = "red", lty = 2, lwd = 2)
+text(1, rhoc[1], as.character(1), pos = 3)
+plot(accu, type = "b", ylim = c(.9991,1), ylab = "accuracy estimate")
+abline(h = fm$accuracy, col = "red", lty = 2, lwd = 2)
+obs <- c(15,46)
+text(obs, accu[obs], as.character(obs), pos = 1)
+obs <- c(27,60)
+text(obs, accu[obs], as.character(obs), pos = 3)
+plot(r, type = "b", ylim = c(.935,.965), ylab = "precision estimate")
+abline(h = fm$precision, col = "red", lty = 2, lwd = 2)
+text(1, r[1], as.character(1), pos = 3)
