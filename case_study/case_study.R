3427574474a2e6cb8858426f61250cdbb9578ead
Felipe Osorio <faosorios@gmail.com>
2018-10-23 12:18:24 -0300

    Add files via upload

--- case_study/case_study.R
+++ case_study/case_study.R
@@ -1,28 +1,18 @@
 ## loading dataset and reading R sources
 load("../data/PSG.rda")
 source("../code/ccc.R")
+source("../code/poa.R")
 source("../code/ccc.influence.R")
+source("../code/poa.influence.R")
 
-## Fig 1
-#pdf(width = 10)
-par(mfrow = c(1,2))
-o <- boxplot(PSG$manual, plot = FALSE) # saving summaries
-o <- c(min(PSG$manual), o$stats, max(PSG$manual))
-boxplot(PSG$manual, col = "gray", xlab = "Manual", ylim = c(0,6.2), boxwex = 0.5)
-text(rep(1.3,7), round(o,3), round(o,3), cex = 0.7)
-o <- boxplot(PSG$automated, plot = FALSE) # saving summaries
-o <- c(min(PSG$automated), o$stats, max(PSG$automated))
-boxplot(PSG$automated, col = "gray", xlab = "Automated", ylim = c(0,6.2), boxwex = 0.5)
-text(rep(1.3,7), round(o,3), round(o,3), cex = 0.7)
-
-## Fig 2.a
+## Fig 1.a
 par(pty = "s")
 plot(automated ~ manual, data = PSG, xlim = c(0,7), ylim = c(0,7))
 abline(c(0,1), col = "red", lty = 2, lwd = 2)
 obs <- c(1,30,79)
 text(PSG$manual[obs], PSG$automated[obs], as.character(obs), pos = 3)
 
-## Fig 2.b
+## Fig 1.b
 average <- apply(PSG[,1:2], 1, mean)
 diff <- PSG$manual - PSG$automated
 qz <- qnorm(c(0.025, 0.975))
@@ -33,6 +23,16 @@ abline(h = mean(diff), col = "red", lwd = 2)
 abline(h = c(limits), col = "red", lty = 2, lwd = 2)
 obs <- c(1,30,35,79)
 text(average[obs], diff[obs], as.character(obs), pos = 3)
+
+## Fig 1.c
+par(pty = "s")
+diff <- PSG$manual - PSG$automated
+o <- boxplot(PSG$manual, plot = FALSE) # saving summaries
+o <- c(min(PSG$manual), o$stats, max(PSG$manual))
+o <- o[-3] # removing quantile 25%
+boxplot(diff, col = "gray", xlab = "Difference between methods", ylim = c(-4,4), boxwex = 0.5)
+text(rep(1.3,6), round(o,3), round(o,3), cex = 0.7)
+
 
 ## compute CCC estimate and confidence interval
 fm <- fit.ccc(~ manual + automated, data = PSG)
@@ -55,7 +55,7 @@ f0 <- fit.ccc(~ manual + automated, data = PSG, subset = -c(1,30,79))
 f0 # print results
 confint(f0, method = "asymp") # print 95% CI
 
-# Output: (2nd row of Table 2)
+# Output: (2nd row of Table 1)
 #Call:
 #fit.ccc(x = ~manual + automated, data = PSG, subset = -c(1, 30, 79))
 #
@@ -66,7 +66,7 @@ confint(f0, method = "asymp") # print 95% CI
 #    parameter         SE     lower    upper
 #CCC 0.8602722 0.02795145 0.8054883 0.915056
 
-## Fig 3
+## Fig 2
 nobs <- 82 # nrow(PSG)
 rhoc <- rep(0, nobs)
 for (i in 1:nobs)
@@ -77,13 +77,25 @@ plot(rhoc, type = "b", ylim = c(.64,.76), ylab = "CCC estimate")
 abline(h = fm$ccc, col = "red", lty = 2, lwd = 2)
 text(obs, rhoc[obs], as.character(obs), pos = 3)
 
+## Fig 3
+poac <- rep(0, nobs)
+for (i in 1:nobs) {
+  f1 <- fit.ccc(x = ~manual + automated, data = PSG, subset = -i)$ccc
+  poac[i] <- confint.poa(f1, level = 0.95, cad = 2)[1]
+}
+obs <- c(1,30,35,79)
+#pdf(width = 11, height = 5)
+plot(rhoc, type = "b", ylab = "Estimated probability of agreement")
+abline(h = confint.poa(fm)[1], col = "red", lty = 2, lwd = 2)
+text(obs, poac[obs], as.character(obs), pos = 3)
+
 ## plotting influence measures..
 
-## Fig 6.a
+## Fig 7.a
 z <- influence.ccc(fm, method = "normal")
 plot(z, idn = 4)
 
-## Fig 6.b
+## Fig 7.b
 hmax <- z$hmax
 h2nd <- z$vectors[,2]
 obs  <- c(1,30,35,79)
@@ -94,10 +106,26 @@ abline(h = cut1, col = "red", lty = 2, lwd = 2)
 abline(v = cut2, col = "red", lty = 2, lwd = 2)
 text(h2nd[obs], hmax[obs], as.character(obs), pos = 3)
 
-## Fig 6.c
+## Fig 7.c
 z <- influence.ccc(fm, method = "FI")
 plot(z)
 
-## Fig 6.d
+## Fig 7.d
 z <- influence.ccc(fm, method = "SI")
+plot(z)
+
+## Fig 8.a
+z <- influence.poa(fm, method = "normal")
+plot(z)
+
+## Fig 8.b
+z <- influence.poa(fm, method = "conformal")
+plot(z)
+
+## Fig 8.c
+z <- influence.poa(fm, method = "FI")
+plot(z)
+
+## Fig 8.d
+z <- influence.poa(fm, method = "SI")
 plot(z)
